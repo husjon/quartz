5b32bbe0ebf484e822395438197c750245da04fd
Jon Erling Hustadnes <JonErling.Hustadnes@gmail.com>
2023-08-31 17:50:41 +0200

    added pre-publish hooks

--- .helper_scripts/publish.sh
+++ .helper_scripts/publish.sh
@@ -1,8 +1,23 @@
 #!/bin/bash
 
-export BASE_DIR="$(dirname "$(realpath "$0")")"
-export QUARTZ_FOLDER="$(realpath ${BASE_DIR}/..)"
+set -e  # Exit on Error
 
-cd "${QUARTZ_FOLDER}" || exit 1
+(
+    export BASE_DIR="$(dirname "$(realpath "$0")")"
+    export QUARTZ_FOLDER="$(realpath ${BASE_DIR}/..)"
 
-npx quartz sync || read -r
+    touch ~/.quartz_publish.env
+    source ~/.quartz_publish.env
+
+    # Pre-Publish hooks
+    cd "${BASE_DIR}" || exit 1
+    for f in ./publish.d/*; do
+        echo "Running pre-publish hook: $f"
+        ${f} 2>&1
+        echo
+    done
+
+    cd "${QUARTZ_FOLDER}" || exit 1
+
+    npx quartz sync || read -r
+) > /tmp/quartz_pulish.log
