f8c80f68483b4bedc8ebc14f2a2a5d69f048d1e8
Jon Erling Hustadnes <JonErling.Hustadnes@gmail.com>
2023-11-01 15:26:49 +0100

    some linting and formatting

--- .helper_scripts/publish.d/10-update-book-list.py
+++ .helper_scripts/publish.d/10-update-book-list.py
@@ -1,18 +1,19 @@
 #!/usr/bin/env python
 
-import datetime
-import yaml
 import glob
 import os
 import re
 
-HOME=os.environ.get('HOME')
-quartz_content_folder=os.environ.get('QUARTZ_FOLDER') + '/content'
-obsidian_vault_folder=os.environ.get('OBSIDIAN_VAULT_FOLDER')
-quartz_books_summary_file = f'{quartz_content_folder}/Books.md'
-books_folder = f'{obsidian_vault_folder}/Books'
+import yaml
 
-frontmatter_pattern = re.compile(r'''
+HOME = os.environ.get("HOME")
+quartz_content_folder = os.environ.get("QUARTZ_FOLDER") + "/content"
+obsidian_vault_folder = os.environ.get("OBSIDIAN_VAULT_FOLDER")
+quartz_books_summary_file = f"{quartz_content_folder}/Books.md"
+books_folder = f"{obsidian_vault_folder}/Books"
+
+frontmatter_pattern = re.compile(
+    r"""
 ---\n
 (?P<content>
 (.*?|\n)+
@@ -22,39 +23,47 @@ tags:
 (.*?|\n)+
 )
 ---\n
-''', flags=re.VERBOSE)
+""",
+    flags=re.VERBOSE,
+)
 
 books = []
-#for book in sorted(glob.glob('*.md', root_dir=books_folder)):
-for book in sorted(glob.glob(f'{books_folder}/*.md')):
-    with open(f'{book}', 'r') as f:
+# for book in sorted(glob.glob('*.md', root_dir=books_folder)):
+for book in sorted(glob.glob(f"{books_folder}/*.md")):
+    with open(f"{book}", "r", encoding="utf-8") as f:
         content = f.read()
 
     match = re.match(pattern=frontmatter_pattern, string=content)
-    frontmatter_content = match.groupdict().get('content')
+    frontmatter_content = match.groupdict().get("content")
 
     book_frontmatter = yaml.safe_load(frontmatter_content)
 
     books.append(book_frontmatter)
 
 
-with open(quartz_books_summary_file, 'w+') as fh:
+with open(quartz_books_summary_file, "w+", encoding="utf-8") as fh:
     output = [
-        '---',
+        "---",
         'title: "Books"',
-        'tags: books',
-        '---',
-        '',
+        "tags: books",
+        "---",
+        "",
     ]
-    output.append(f'| Title | Author | My rating | Read (Year-Month-Day) |')
-    output.append(f'| ----- | ------ | --------- | ---- |')
+    output.append("| Title | Author | My rating | Read (Year-Month-Day) |")
+    output.append("| ----- | ------ | --------- | ---- |")
     for book in books:
-        if "currently-reading" in book['tags']:
-            book['date_read'] = 'Currently Reading'
+        if "currently-reading" in book["tags"]:
+            book["date_read"] = "Currently Reading"
         else:
-            book['date_read'] = book['date_read'].strftime('%Y-%m-%d')
+            book["date_read"] = book["date_read"].strftime("%Y-%m-%d")
 
-        output.append('| {title:<60} | {author:<30} | {rating} | {date_read:<10} |'.format(**book))
+        line = [
+            f"{book['title']:<60}",
+            f"{book['author']:<30}",
+            f"{book['rating']}",
+            f"{book['date_read']:<10}",
+        ]
 
+        output.append(f"| {' | '.join(line)} |")
 
-    fh.write('\n'.join(output))
+    fh.write("\n".join(output))
