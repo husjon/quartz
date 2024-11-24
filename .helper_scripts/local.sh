075981423d192706450a8ee15473d6aed0634a97
Jon Erling Hustadnes <jonerling.hustadnes@gmail.com>
2024-11-24 23:35:29 +0100

    local.sh: replace content folder with symlink
    content folder is restored after HEAD after exit

--- .helper_scripts/local.sh
+++ .helper_scripts/local.sh
@@ -9,12 +9,16 @@ source ./lib/arg_parse.sh
 PID_FILE=/tmp/obsidian_quartz_local_server.pid
 
 cleanup() {
+    cd "${QUARTZ_PATH}"
+
     QUARTZ_PID=$(cat "${PID_FILE}" 2>/dev/null)
     kill ${QUARTZ_PID} >/dev/null 2>&1
     rm -f "${PID_FILE}"
+
+    git checkout .
 }
 
-trap cleanup EXIT TERM INT
+trap cleanup TERM INT
 
 if [[ "$STOP_LOCAL_SERVER" = true ]]; then
     cleanup
@@ -23,17 +27,19 @@ fi
 
 if [[ -f "${PID_FILE}" ]]; then
     cleanup
-    sleep 0.5
+    sleep 3
 fi
 
 test -z $QUARTZ_VAULT_PATH && echo "QUARTZ_VAULT_PATH not set" && exit 1
 test -z $QUARTZ_PATH && echo "QUARTZ_PATH not set" && exit 1
 
-pushd ..
+cd "${QUARTZ_PATH}"
+rm -rf content/
+ln -rsf "${QUARTZ_VAULT_PATH}/" "${QUARTZ_PATH}/content"
+
 timeout --preserve-status 1h npx quartz build --serve &
 QUARTZ_PID=$!
 echo ${QUARTZ_PID} >"${PID_FILE}"
-popd
 
 notify-send -u low "Quartz" "Starting local server"
 sleep 3 # allow the server to start up before checking
